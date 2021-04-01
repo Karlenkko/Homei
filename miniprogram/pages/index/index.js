36d80ac715da5ef1a3bc585a6d220080c5fb920b
Karlenkko <shihangzhou0517@gmail.com>
2021-04-01 04:33:46 +0200

    add horizontal menu

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,32 +1,104 @@
 // miniprogram/pages/index/index.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
   },
 
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  // 滚动切换标签样式
+ switchTab:function(e){
+  this.setData({
+   currentTab:e.detail.current
+  });
+  this.checkCor();
+ },
+ // 点击标题切换当前页时改变样式
+ switchNav:function(e){
+  var cur=e.currentTarget.dataset.current;
+  if(this.data.currentTaB==cur){return false;}
+  else{
+   this.setData({
+    currentTab:cur
+   })
+  }
+ },
+ //判断当前滚动超过一屏时，设置tab标题滚动条。
+ checkCor:function(){
+  if (this.data.currentTab>4){
+  this.setData({
+   scrollLeft:300
+  })
+  }else{
+  this.setData({
+   scrollLeft:0
+  })
+  }
+ },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this; 
+    // 高度自适应
+    wx.getSystemInfo( { 
+    success: function( res ) { 
+      var clientHeight=res.windowHeight,
+      clientWidth=res.windowWidth,
+      rpxR=750/clientWidth;
+      var calc=clientHeight*rpxR-180;
+      console.log(calc)
+      that.setData( { 
+      winHeight: calc 
+      }); 
+    } 
+    });
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    // wx.createSelectorQuery().select('.scroll-view').boundingClientRect((rect)=>{
+    //   this.data.scrollViewWidth = 800
+    // }).exec()
   },
-
+  // switchClass(){
+  //   let offsetLeft = e.currentTarget.offsetLeft
+  //   this.setData({
+  //   scrollLeft: offsetLeft - this.data.scrollViewWidth/2
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
 
   /**
@@ -62,5 +134,6 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  footerTap:app.footerTap
 })
