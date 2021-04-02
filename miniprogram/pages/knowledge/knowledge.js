05457269e48550cac8fc6a7418c48e8e6a44876d
unknown <unknown@example.com>
2021-04-02 10:38:47 +0200

    update knowledge page

--- miniprogram/pages/knowledge/knowledge.js
+++ miniprogram/pages/knowledge/knowledge.js
@@ -1,4 +1,7 @@
 // miniprogram/pages/knowledge.js
+const db = wx.cloud.database()
+const Questionnaire = db.collection('Questionnaire')
+
 Page({
 
   /**
@@ -7,60 +10,12 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // start the questionnaire
+  start(){
+    Questionnaire.get({
+      success: function(res){
+        console.log(res.data[0]);
+      }
+    })
   }
 })
