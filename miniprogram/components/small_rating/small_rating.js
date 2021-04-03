3706ab5940028ad22c86997d913e7ce92860d079
Karlenkko <shihangzhou0517@gmail.com>
2021-04-03 21:15:27 +0200

    add small rating

---
+++ miniprogram/components/small_rating/small_rating.js
@@ -0,0 +1,26 @@
+// components/small_rating/small_rating.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    rating: {
+      type: Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    yesSrc: 'images/full_star.png',
+    noSrc: 'images/empty_star.png',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
