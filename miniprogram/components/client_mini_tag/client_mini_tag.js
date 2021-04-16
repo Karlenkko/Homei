28f92640db8a9dbda06d418697079005f551c011
Dingwanoscartan <tantuoyuan2333@gmail.com>
2021-04-16 10:37:15 +0200

    Add bigger client tag in the first page

---
+++ miniprogram/components/client_mini_tag/client_mini_tag.js
@@ -0,0 +1,28 @@
+// components/client_mini_tag/client_mini_tag.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type: String,
+    },
+    color: {
+      type: String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
